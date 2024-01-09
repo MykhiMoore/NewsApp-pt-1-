2fb2f7aa81e1afa28bd7d6334ff9c6a83031f3cf
MykhiMoore <moore_mykhi@student.mahoningctc.com>
2024-01-09 12:48:56 -0500

    new changes done with pt 2

---
+++ NewsApp/Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using NewsApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Services
+{
+        public class ApiService
+        {
+        public async Task<Root> GetNews()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=24b4bd082019e9e8c693bd379fa5256c&topic=breaking-news&lang=en&topic=sports");
+
+            //deserialization 
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
