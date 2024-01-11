ed91575b80d6318236954d8ca29f54822ab2950a
MykhiMoore <moore_mykhi@student.mahoningctc.com>
2024-01-11 12:12:22 -0500

    New changes finished pt3

--- NewsApp/Pages/NewsPage.xaml.cs
+++ NewsApp/Pages/NewsPage.xaml.cs
@@ -6,20 +6,46 @@ namespace NewsApp.Pages;
 public partial class NewsPage : ContentPage
 {
     public List<Article> ArticlesList { get; set; }
-	public NewsPage()
+    public List<Category> CategorysList = new List<Category>()
+    {
+    new Category(){Name = "breaking-news"},
+    new Category(){Name = "world"},
+    new Category(){Name = "nation"},
+    new Category(){Name = "business"},
+    new Category(){Name = "technology"},
+    new Category(){Name = "entertainment"},
+    new Category(){Name = "sports"},
+    new Category(){Name = "science"},
+    new Category(){Name = "health"},
+    };
+    public NewsPage()
 	{
 		InitializeComponent();
         ArticlesList = new List<Article>();
-	}
+        CvCategories.ItemsSource = CategorysList;
 
+	}
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+        await PassCategory("breaking-news");
+    }
+
+    public async Task PassCategory(string categoryName)
+    {
+        CvNews.ItemsSource = null;
+        ArticlesList.Clear();
         ApiService apiService = new ApiService();
-        var newsResult = await apiService.GetNews();
-        foreach ( var item in newsResult.Articles)
+        var newsresult = await apiService.GetNews(categoryName);
+        foreach (var item in newsresult.Articles)
         {
             ArticlesList.Add(item);
         }
+        CvNews.ItemsSource = ArticlesList;
     }
-}
+        private async void CvCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = e.CurrentSelection.FirstOrDefault() as Category;
+            await PassCategory(selectedItem.Name); 
+        }
+    }
--- NewsApp/Services/ApiService.cs
+++ NewsApp/Services/ApiService.cs
@@ -10,10 +10,10 @@ namespace NewsApp.Services
 {
         public class ApiService
         {
-        public async Task<Root> GetNews()
+        public async Task<Root> GetNews(string categoryName)
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=24b4bd082019e9e8c693bd379fa5256c&topic=breaking-news&lang=en&topic=sports");
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=24b4bd082019e9e8c693bd379fa5256c&topic="+categoryName.ToLower());
 
             //deserialization 
             return JsonConvert.DeserializeObject<Root>(response);
